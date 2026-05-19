a815a5cba83dcc6d08ddf34dfe61869c21342263
Crocobob <mjdaniel911@naver.com>
2026-05-19 15:42:43 +0900

    UI Loader구현

---
+++ Assets/UI/Scripts/Popup/Friend_MainWindow.cs
@@ -0,0 +1,20 @@
+using DefaultNamespace.UI.Scripts;
+using UnityEngine;
+
+namespace DefaultNamespace.UI.Scripts
+{
+    public class Friend_MainWindow : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/UI/Scripts/UI_IDataReceiver.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace DefaultNamespace.UI.Scripts
+{
+    /// <summary>
+    /// UILoader로부터 타입 안정성을 보장받으며 데이터를 전달받기 위한 공통 인터페이스입니다.
+    /// 다중 데이터의 경우 ValueTuple 형태(예: IUIDataReceiver<(StructA, ClassB)>)로 구현이 가능합니다.
+    /// </summary>
+    public interface UI_IDataReceiver<in T>
+    {
+        void ReceiveData(T data);
+    }
+}
