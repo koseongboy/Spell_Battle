39f3051fe3b11f720cb1ab5ce722f0f932f3932b
Crocobob <mjdaniel911@naver.com>
2026-05-28 20:59:48 +0900

    친구 Detail 팝업

--- Assets/UI/Scripts/Controller/FriendPopupController.cs
+++ Assets/UI/Scripts/Controller/FriendPopupController.cs
@@ -13,7 +13,8 @@ namespace DefaultNamespace
         
         
         private Friend_MainWindow ui_mainWindow;
-        private Friend_SearchWindow _uiSearchWindowWindow;
+        private Friend_SearchWindow ui_SearchWindowWindow;
+        private Friend_DetailWindow ui_detailWindow;
         
         private bool isMainWindowOpen = false;
         private bool isSearchWindowOpen = false;
@@ -36,6 +37,7 @@ namespace DefaultNamespace
             ui_mainWindow.UpdateUI(myProfile, friendList);
         }
         
+        
         public void ToggleOnOff_MainWindow() {
             if (!isMainWindowOpen) {
                 UILoader.Instance.ShowUI("Friend_MainWindow");
@@ -50,12 +52,6 @@ namespace DefaultNamespace
         public void OpenAlert() {
             
         }
-
-        public void ShowFriendDetail(int userId) {
-            // TODO : 유저 ID를 바탕으로 서버에서 상세 정보를 긁어와서 새로운 팝업 띄우기
-            Debug.Log($"[유저 ID: {userId}] 친구 상세정보 창 팝업 Controller에서 실행됨!");
-        }
-        
         
         private (int, int, string) LoadMyProfile() 
         {
@@ -76,15 +72,14 @@ namespace DefaultNamespace
         
         #endregion
         
-        
         #region Search
         public void RegisterSearchWindow(Friend_SearchWindow ui) 
         {
-            _uiSearchWindowWindow = ui;
+            ui_SearchWindowWindow = ui;
 
             // 이벤트 구독
-            _uiSearchWindowWindow.OnSubmit_Search += RequestSearchUser;
-            _uiSearchWindowWindow.OnClick_AddFriend += RequestSendFriend;
+            ui_SearchWindowWindow.OnSubmit_Search += RequestSearchUser;
+            ui_SearchWindowWindow.OnClick_AddFriend += RequestSendFriend;
         }
         
         public void ToggleOnOff_SearchWindow() {
@@ -120,11 +115,11 @@ namespace DefaultNamespace
                 };
                 
                 // View에 데이터 전달하여 결과 띄우기
-                _uiSearchWindowWindow.ShowSearchResult(resultList);
+                ui_SearchWindowWindow.ShowSearchResult(resultList);
             }
             else 
             {
-                _uiSearchWindowWindow.ClearSearchResult();
+                ui_SearchWindowWindow.ClearSearchResult();
                 CommonUIController.Instance.ShowBlackAlert("해당 이름을 가진 유저가 없습니다.");
             }
         }
@@ -136,6 +131,60 @@ namespace DefaultNamespace
             Debug.Log($"서버에 유저 ID [{targetUserId}] 에게 친구 요청 패킷을 발송합니다.");
 
             // TODO: 친구 요청 서버 API 호출 로직
+        }
+        #endregion
+        
+        #region DetailWindow
+        public void RegisterDetailWindow(Friend_DetailWindow ui) 
+        {
+            ui_detailWindow = ui;
+        }
+
+        // 닫기 함수
+        public void CloseDetailWindow() 
+        {
+            UILoader.Instance.HideUI("Friend_DetailWindow");
+        }
+
+        // 🌟 메인 친구 리스트에서 특정 친구의 Piece를 클릭했을 때 실행됨
+        public void ShowFriendDetail(int userId) 
+        {
+            // 1. 내가 가지고 있는 전체 친구 리스트 데이터 중에서, 해당 ID를 가진 유저 정보를 찾습니다.
+            var currentFriendList = LoadFriendList(); // 실제로는 클래스 전역에 캐싱해둔 리스트를 사용해야 합니다.
+    
+            // LINQ 등을 사용해 ID 매칭
+            FriendDataForUI targetFriendData = currentFriendList.Find(f => f.userId == userId);
+
+            // 2. 만약 데이터를 찾았다면, UILoader의 <T> 제네릭 기능을 이용해 창을 띄우면서 데이터를 쏴줍니다.
+            if (targetFriendData.userId != 0) // struct 초기화 검증 (또는 별도 예외처리)
+            {
+                UILoader.Instance.ShowUI<FriendDataForUI>("Friend_DetailWindow", targetFriendData);
+            }
+            else
+            {
+                Debug.LogWarning("해당 친구의 데이터를 찾을 수 없습니다.");
+            }
+        }
+
+        // [게임 초대] 버튼 클릭 시
+        public void RequestInviteGame(int targetUserId)
+        {
+            Debug.Log($"서버를 통해 유저 ID [{targetUserId}] 에게 게임 초대장(로비 코드 등)을 발송합니다.");
+            // TODO: Unity Relay/Lobby의 '초대 코드(Join Code)'를 대상 유저에게 전송하는 API 연동
+    
+            // 필요 시 초대 완료 후 알림 팝업
+        }
+
+        // [친구 삭제] 버튼 클릭 시
+        public void RequestDeleteFriend(int targetUserId)
+        {
+            Debug.Log($"서버에 유저 ID [{targetUserId}] 친구 삭제를 요청합니다.");
+    
+            // TODO: UGS Friends API를 통한 삭제 연동
+    
+            // 삭제가 완료되면 상세창을 닫고 메인 리스트를 갱신합니다.
+            CloseDetailWindow();
+            UpdateUI_MainWindow();
         }
         #endregion
     }
