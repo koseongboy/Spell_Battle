222db99928f8987101fee59c7f191f7bd8720fc3
Crocobob911 <mjdaniel911@naver.com>
2026-06-09 12:18:34 +0900

    게임 종료 UI - Draw 추가

--- Assets/UI/Scripts/Top/GameEnd_Top.cs
+++ Assets/UI/Scripts/Top/GameEnd_Top.cs
@@ -1,26 +1,39 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace DefaultNamespace
 {
-    public class GameEnd_Top : MonoBehaviour, UI_ILayerInfo, UI_IDataReceiver<bool>
+    public enum GameEndType {
+        Win,
+        Lose,
+        Draw
+    }
+    
+    
+    public class GameEnd_Top : MonoBehaviour, UI_ILayerInfo, UI_IDataReceiver<GameEndType>
     {
         public EUILayer TargetLayer => EUILayer.Top;
 
         public GameObject winUI;
-        public GameObject defeatUI;
+        public GameObject loseUI;
+        public GameObject drawUI;
         
-        public void ReceiveData(bool isWin) {
-            if (isWin) {
+        public void ReceiveData(GameEndType gameEndType) {
+            winUI.SetActive(false);
+            loseUI.SetActive(false);
+            drawUI.SetActive(false);
+            
+            if (gameEndType == GameEndType.Win) {
                 winUI.SetActive(true);
-                defeatUI.SetActive(false);
-            }
-            else {
-                winUI.SetActive(false);
-                defeatUI.SetActive(true);
+            }else if (gameEndType == GameEndType.Lose) {
+                loseUI.SetActive(true);
+            }else if (gameEndType == GameEndType.Draw) {
+                drawUI.SetActive(true);
             }
         }
 
         public void OnClick_Next() {
+            Debug.Log($"여기서 메인 로비로 돌아가는 로직");
             // TODO : 게임 다 끝내고 메인 로비로 돌아가는 로직
         }
     }
